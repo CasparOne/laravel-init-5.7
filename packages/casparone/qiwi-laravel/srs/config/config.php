f982bc94f5a1e589df86d92451e4948034c87409
CasparOne <vvolodin.victor@gmail.com>
2019-09-02 18:10:21 +0300

    Qiwi dev

---
+++ packages/casparone/qiwi-laravel/srs/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | QIWI Secret Key
+    |--------------------------------------------------------------------------
+    |
+    | All Merchant's requests are authorised by secret key
+    | Auth parameter set in Authorization header which value formed as “Bearer SECRET_KEY”
+    | Public key  (PUBLIC_KEY) uses for make invoices via web-form
+    |
+    */
+
+    'secret_key' => 'paste_here_secret_key',
+
+];
